1cac21428d3fcf1f432850b4ef169993a87b1051
Sara Nabih <saranabih@gmail.com>
2025-03-04 10:35:57 +0200

    Enhanced Updates were added

--- Notification System/NotificationSystem.cs
+++ Notification System/NotificationSystem.cs
@@ -8,15 +8,30 @@ namespace Notification_System
        
         List<User> Subscribers = new List<User>();
 
+        public bool IsSubscribed(User user) => Subscribers.Contains(user);
+
+        public bool HasSubscribers() => Subscribers.Count > 0;
+
         public void Subscribe(User user)
         {
-            OnNotification += user.ReceiveNotification;
-            Subscribers.Add(user);
-            Console.WriteLine($"{user.Name} has Subscribed to Notifications :-)");
+            if (!IsSubscribed(user))
+            {
+                OnNotification += user.ReceiveNotification;
+                Subscribers.Add(user);
+                Console.WriteLine($"{user.Name} has Subscribed to Notifications :-)");
+            }
+            else
+                Console.WriteLine($"{user.Name} has Subscribed to Notifications Before!");
+                
         }
 
         public void Unsubscribe(User user) 
         {
+            if(!IsSubscribed(user))
+            {
+                Console.WriteLine($"{user.Name} has UnSubscribed to Notifications Before!");
+                return;
+            }
             OnNotification -= user.ReceiveNotification;
             Subscribers.Remove(user);
             Console.WriteLine($"{user.Name} has unSucrided from Notifications :-(");
@@ -24,6 +39,11 @@ namespace Notification_System
 
         public void SendNotification(string Message)
         {
+            if (!HasSubscribers())
+            {
+                Console.WriteLine("No Subscribers to Send Notification to!");
+                return;
+            }
             Console.WriteLine("\nSending Notification...");
             OnNotification?.Invoke(Message);
         }
